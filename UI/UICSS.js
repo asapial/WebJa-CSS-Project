10217f1dbe6e016288565276f5c568af88dbba05
Abu Syeed Abdullah <72955464+asapial@users.noreply.github.com>
2024-07-08 04:42:06 +0600

    laptop

--- UI/UICSS.js
+++ UI/UICSS.js
@@ -10,46 +10,9 @@ function caretSideBar()
 function makeHeader()
 {
     document.getElementById("hamBurgerButton").style.display="none";
-    document.getElementById("headerList").style.display="block";
-    const head=document.getElementById("headerAll");
-    head.style.display="flex";
+    document.getElementById("headerAll").style.display="none";
+    document.getElementById("header500").style.display="block";
+    // document.getElementById("headerButton").style.display="none";
 
-    const custom_style={
-    display: flex;
-    width: 100%;
-    background-color: rgb(79, 79, 79);
-    flex-directon:column;
-    justify-content: space-between;
-    align-items: center;
-    font-family: 'Times New Roman', Times, serif;
-    border-bottom: 2px solid rgb(46, 46, 46);
-    position: relative;
-    z-index: 1000;
-    }
-    
-    Object.assign(head.style,custom_style);
 
 }
-
-// header
-// {
-//     display: flex;
-//     height: min(10vw,80px);
-//     width: 100%;
-//     background-color: rgb(79, 79, 79);
-//     justify-content: space-between;
-//     align-items: center;
-//     font-family: 'Times New Roman', Times, serif;
-//     border-bottom: 2px solid rgb(46, 46, 46);
-//     position: relative;
-//     z-index: 1000;
-// }
-
-// const ele=document.getElementById("ele_id");
-// const custom_style={
-//     display: "block",
-//     color: "red"
-// }
-
-// //Object.assign():
-// Object.assign(ele.style,custom_style);
