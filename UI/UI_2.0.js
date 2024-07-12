f7b8f2b0c3073d1394c8fb0198c199832b0c269b
Abu Syeed Abdullah <72955464+asapial@users.noreply.github.com>
2024-07-12 10:16:12 +0600

    laptop

---
+++ UI/UI_2.0.js
@@ -0,0 +1,23 @@
+function makeWrapeer()
+{
+    const wrap=document.getElementById("sideBar");
+    wrap.style.display="block";
+
+    const container=document.getElementById("container");
+
+    // container.style.backgroundColor="red";
+    container.style.gridTemplateColumns="auto  repeat(11,1fr)"
+    container.style.gridTemplateAreas=
+    `
+    "h h h h h h h h h h h h"
+    "s c c c c c c c c c c c"
+    "s c c c c c c c c c c c"
+    `;
+
+    const links = document.querySelectorAll('.wrapper a');
+    links.forEach(link => {
+      link.style.fontSize = '2vw';
+      link.style.color = '#ECF0F1';
+      link.style.textDecoration = 'none';
+    });
+}
